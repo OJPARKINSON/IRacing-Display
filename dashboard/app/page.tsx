ef4dce3fb5db186b154a427c1e678201d525f6e9
Oliver Parkinson <olliepar01@gmail.com>
2024-08-24 16:55:54 +0100

    added chart and

---
+++ dashboard/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import BarChart from "@/components/charts/barChart";
+import useSWRSubscription from "swr/subscription";
+
+export default function DashboardPage() {
+  const { data, error } = useSWRSubscription(
+    "ws://localhost:8080/ws",
+    (key, { next }) => {
+      const socket = new WebSocket(key);
+      socket.addEventListener("message", (event) => next(null, event.data));
+      return () => socket.close();
+    }
+  );
+
+  const speed = [{ name: "test", speed: data }];
+
+  return (
+    <>
+      <BarChart data={speed} />
+    </>
+  );
+}
